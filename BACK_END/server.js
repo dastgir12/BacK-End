d07485d48d49f34f6557feeead481d51ec9e9a32
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-16 01:09:10 +0500

    init

---
+++ BACK_END/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectdb= require('./config/db')
+const app= express();
+connectdb();
+app.use(express.json({extended:false}));
+const PORT= process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send('Api Running');
+})
+app.use('/api/reg',  require('./routes/reg'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/forget',  require('./routes/forget-password'));
+app.use('/api/reset-password',  require('./routes/reset-password'));
+app.use('/api/profile',require('./routes/profile'));
+app.use('/api/posts' , require('./routes/posts'))
+app.listen(PORT,()=>{
+    console.log(`Server Started At Port ${PORT}`);
+});
